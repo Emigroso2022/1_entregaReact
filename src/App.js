dd796883aabe338f3ce8dcee08489f9626c051df
Emigroso2022 <dalupsystems.2021@gmail.com>
2022-10-22 21:29:31 -0300

    Entrega1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1>Hola Mundo !!. </h1>
+      <li>Inicio </li>
+      <li>Quienes somos </li>
+      <li>Servicios </li>
+      <li>Contacto </li>
+    </div>
+  );
+}
+
+export default App;
